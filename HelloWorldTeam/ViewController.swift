a9f1abbb836fdb37a273b91795f129860f05babd
David Huang <David@davids-macbook-pro.local>
2017-05-11 15:44:11 -0500

    add one line

--- HelloWorldTeam/ViewController.swift
+++ HelloWorldTeam/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello World")
+        print("haha")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
